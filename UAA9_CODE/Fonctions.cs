ad90a43d5f9c76ce936c1cb5c6bef6eebf0a05c1
200046 - TASNIER MARCEL <200046@site.asty-moulin.be>
2026-03-03 16:04:07 +0100

    03-03-2026

---
+++ UAA9_CODE/Fonctions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace UAA9_CODE
+{
+    internal class Fonctions
+    {
+        /// <summary>
+        /// Creation du talon de dominos
+        /// </summary>
+        public static void creationTalon()
+        {
+            string[] tableauDominos = new string[28]; // Il y a 28 dominos dans un jeu standard
+            int compteurDomino = 0;
+
+            for (int pointsFaceA = 0; pointsFaceA <= 6; pointsFaceA++)
+            {
+                for (int pointsFaceB = pointsFaceA; pointsFaceB <= 6; pointsFaceB++)
+                {
+                    string dominoActuel = "[" + pointsFaceA + "|" + pointsFaceB + "]";
+                    tableauDominos[compteurDomino] = dominoActuel;
+                    compteurDomino++;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Mélange aléatoire
+        /// </summary>
+        public static void melangeAleatoire()
+        {
+            string[] tableauDominos = new string[28]; // Il y a 28 dominos dans un jeu standard
+            int compteurDomino = 0;
+            // Mélange aléatoire
+            Random random = new Random();
+
+            for (int i = 0; i < tableauDominos.Length; i++)
+            {
+                int indexAleatoire = random.Next(i, tableauDominos.Length);
+
+                // Échange des valeurs
+                string temp = tableauDominos[i];
+                tableauDominos[i] = tableauDominos[indexAleatoire];
+                tableauDominos[indexAleatoire] = temp;
+            }
+
+            // Affichage après mélange
+            Console.WriteLine("Dominos mélangés :");
+            foreach (string domino in tableauDominos)
+            {
+                Console.WriteLine(domino);
+            }
+        }
+    }
+}
---
+++ UAA9_CODE/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UAA9_CODE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
